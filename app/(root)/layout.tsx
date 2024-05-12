b9aba0d3fec0e4bc72a9fcc3d713c0f91d256da1
Adarsh Prakash Patil <adarshprakashpatil04@gmail.com>
2024-05-12 17:35:34 +0530

    Third Commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,27 @@
-import Sidebar  from "@/components/Sidebar";
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: 'Adrian', lastName: 'Mejia' };
+  const loggedIn = { firstName: "Adrian", lastName: "Mejia" };
 
   return (
-      <main className="flex h-screen w-full font-inter">
+    <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
-          {children}
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          {/* When above md, it is set to hidden for root-layout */}
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
