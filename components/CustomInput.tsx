a55ffb71acfe6bbd672e328bea84e70074473c31
Adarsh Prakash Patil <adarshprakashpatil04@gmail.com>
2024-05-15 10:11:35 +0530

    chore: Update sign-in and sign-up pages with AuthForm component

---
+++ components/CustomInput.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import {
+  FormControl,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { authFormSchema } from "@/lib/utils";
+import { z } from "zod";
+
+interface CustomInputProps {
+  control: Control<z.infer<typeof authFormSchema>>;
+  name: FieldPath<z.infer<typeof authFormSchema>>;
+  label: string;
+  placeholder: string;
+}
+
+const CustomInput = ({
+  control,
+  name,
+  label,
+  placeholder,
+}: CustomInputProps) => {
+  return (
+    <div>
+      <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <div className="form-item">
+            <FormLabel className="form-label">{label}</FormLabel>
+            <div className="flex w-full flex-col">
+              <FormControl>
+                <Input
+                  placeholder={placeholder}
+                  type={name === "password" ? "password" : "text"}
+                  className="input-class"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="form-message mt-2" />
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
+
+export default CustomInput;
